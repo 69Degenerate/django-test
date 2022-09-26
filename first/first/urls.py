7f1f63c7819fb7e916704879021ebf7ec5380191
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-09-26 21:51:41 +0530

    a

--- first/first/urls.py
+++ first/first/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from demo import views
-
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
